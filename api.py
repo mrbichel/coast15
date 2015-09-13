d0e88c751516162f36f871051922949af6fa5fce
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-13 14:34:34 +0200

    Add PEP8 compliancy

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ from flask.ext.cors import CORS
 import pymongo
 from pymongo import MongoClient, GEO2D
 
+
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
     leftSpan = leftMax - leftMin
@@ -33,13 +34,15 @@ db = client.uk_tide
 locations = db.locations
 tide_logs = db.tide_logs
 
+
 @app.route("/")
 def hello():
     return "Tide Data API."
 
+
 @app.route("/loc")
 def location():
-    #return "Hello World!"
+    # return "Hello World!"
     locs = locations.find()
     json_locs = []
 
@@ -48,32 +51,34 @@ def location():
     for loc in locs:
 
         # get interpolated tide level for current time
-        #print(loc);
+        # print(loc);
 
         # get closest measure later than time
         n = tide_logs.find_one({
             "location": ObjectId(loc['_id']),
             "timestamp": {"$gte": d}
-            }, sort=[("timestamp", pymongo.ASCENDING)])
+        }, sort=[("timestamp", pymongo.ASCENDING)])
 
         b = tide_logs.find_one({
             "location": ObjectId(loc['_id']),
             "timestamp": {"$lte": d}
-            }, sort=[("timestamp", pymongo.DESCENDING)])
+        }, sort=[("timestamp", pymongo.DESCENDING)])
 
-##### ------- before ------- (lower than now - DESCENDING) now (greater than now ASCENDING ) ------- next ------
+# ------- before ------- (lower than now - DESCENDING) now (greater than n
         interpolatedHeight = 0
         loc['height'] = 0
 
         if n and b:
             totalTimeDelta = n['timestamp'] - b['timestamp']
-            #print(totalTimeDelta.total_seconds())
+            # print(totalTimeDelta.total_seconds())
 
             nTimeDelta = n['timestamp'] - d
             bTimeDelta = d - b['timestamp']
 
-            nWeight = nTimeDelta.total_seconds()/totalTimeDelta.total_seconds()
-            bWeight = bTimeDelta.total_seconds()/totalTimeDelta.total_seconds()
+            nWeight = nTimeDelta.total_seconds() / \
+                totalTimeDelta.total_seconds()
+            bWeight = bTimeDelta.total_seconds() / \
+                totalTimeDelta.total_seconds()
 
             interpolatedHeight = n['height'] * nWeight + b['height'] * bWeight
             loc['height'] = interpolatedHeight
@@ -81,13 +86,13 @@ def location():
             loc['next'] = n
             loc['prev'] = b
 
-            #print(n['height'])
-            #print(b['height'])
-            #print(interpolatedHeight)
+            # print(n['height'])
+            # print(b['height'])
+            # print(interpolatedHeight)
 
         # use .next to get next one
 
-        #print(tide_logs.find_one({
+        # print(tide_logs.find_one({
         #    "location": ObjectId(loc['_id']),
         #    "timestamp": {"$grt": d}
         #    }).sort("timestamp"))
@@ -100,13 +105,8 @@ def location():
         json_loc = json.loads(json.dumps(loc, default=json_util.default))
         json_locs.append(json_loc)
 
-
-
     return jsonify(locations=json_locs)
 
 
 if __name__ == "__main__":
     app.run(use_debugger=app.debug)
-
-
-
