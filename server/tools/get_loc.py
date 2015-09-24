ae020de9b6b5ec51a4e9a59e4843647cfb7fc2ea
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-24 23:13:52 +0200

    new cities

--- server/tools/get_loc.py
+++ server/tools/get_loc.py
@@ -7,10 +7,14 @@ Request lat long from google.
 import pandas as pd
 import requests
 import json
-import sys
+import time
+# import sys
 
-file_path = "../../coast_shared_data/headlands_lands_end_to_london_clockwise.csv"#sys.argv[1]
-#if file_path == "(../../coast_shared_data/headlands_lands_end_to_london_clockwise.csv":
+file_path = "./allhead.csv"
+# file_path = "./all.csv"
+# wtf
+# sys.argv[1]
+# if file_path == "(../../coast_shared_data/headlands_lands_end_to_london_clockwise.csv":
 #    print("type filename with cityies")
 
 df = pd.DataFrame.from_csv(file_path)
@@ -18,26 +22,32 @@ df = pd.DataFrame.from_csv(file_path)
 
 
 payload = {"address": "Scabbacombe Head", "components": "country:Uk",
-           "key": "AIzaSyAEO74970tLPQqr3K9z_tDj2W1r1mC49NE"}
+           "key": "AIzaSyApa4ZMw-LY5s6i11DxBcivlJB1jzxVKJk"}
 all = []
 for i in df.index:
     try:
         url = "https://maps.googleapis.com/maps/api/geocode/json"
         payload = {"address": i,
-                   "key": "AIzaSyAEO74970tLPQqr3K9z_tDj2W1r1mC49NE"}
+                   "key": "AIzaSyApa4ZMw-LY5s6i11DxBcivlJB1jzxVKJk"}
+        time.sleep(0.1)
         r = requests.get(url, payload)
         v = r.json()
+        status = v['status'] 
+        if status == "ZERO_RESULTS":
+            print(i)
+            continue
         res = v['results'][0]
         latlong = res['geometry']['location']
         name = res['address_components'][0]['long_name']
         latlong["name"] = i
         if name != "United Kingdom":
             all.append(latlong)
-    except:
+    except Exception as e:
+        print(v)
+        print(e)
         print(i)
 
 with open('{}_latlong.json'.format(file_path.replace(".csv", "")), 'w') as fp:
     json.dump(all, fp)
 # vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
 # vim: filetype=python foldmethod=indent
-
